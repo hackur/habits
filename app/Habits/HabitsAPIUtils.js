6be3e03259cee08fb8a330001ed57c695af2eb78
Franky Chung <franchun@gmail.com>
2015-02-28 18:14:10 +0900

    HabitsUtils

--- app/Habits/HabitsAPIUtils.js
+++ app/Habits/HabitsAPIUtils.js
@@ -4,14 +4,15 @@
 
 'use strict';
 
+var HabitsUtils = require('./HabitsUtils');
 var firebaseUtils = require('../shared/firebaseUtils');
 var dateUtils = require('../shared/dateUtils');
 
-function createHabit(user/*: User*/, newHabit/*: string*/) {
-  var key = firebaseUtils.push(user.dataUrl + '/habits', {
-    name: newHabit,
-    streak: 0
-  });
+function createHabit(user: User, newHabit: string) {
+  var key = firebaseUtils.push(
+    user.dataUrl + '/habits',
+    HabitsUtils.getNewHabitData(newHabit)
+  );
 
   return firebaseUtils.set(user.dataUrl + '/data/' + key, {
     start: dateUtils.getCurrentDayKey()
---
+++ app/Habits/HabitsUtils.js
@@ -0,0 +1,17 @@
+/**
+ * @flow
+ */
+
+'use strict';
+
+function getNewHabitData(newHabit: string): Object {
+  return {
+    name: newHabit,
+    streak: 0,
+    last: null
+  };
+}
+
+module.exports = {
+  getNewHabitData
+};
