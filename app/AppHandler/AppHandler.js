e9a994e64373c6c52d3f17bd528dfe39e8023e4f
Franky Chung <franchun@gmail.com>
2015-03-21 18:49:17 +0900

    Use higher order components in AppHandler

--- app/AppHandler/AppHandler.js
+++ app/AppHandler/AppHandler.js
@@ -3,32 +3,46 @@
  */
 
 const React = require('react/addons');
+const Immutable = require('immutable');
 const { RouteHandler, Navigation, State } = require('react-router');
 const isEmpty = require('lodash/lang/isEmpty');
 const reduce = require('lodash/collection/reduce');
+const compose = require('lodash/function/compose');
 
 const AppHandlerViewActionCreators = require('./AppHandlerViewActionCreators');
+const ConnectToStores = require('../ConnectToStores');
+const AddStatics = require('../AddStatics');
 const UserStore = require('../User/UserStore');
-const StoresMixin = require('../StoresMixin');
 const Inside = require('../Inside/Inside');
 const Outside = require('../Outside/Outside');
 
 require('normalize-css/normalize.css');
 require('./AppHandler.less');
 
-const AppHandler = React.createClass({
-  statics: {
+const { PropTypes } = React;
+
+const stores = [UserStore];
+
+const getStateFromStores = () => ({
+      user: UserStore.get()
+});
+
+const statics = {
     willTransitionTo(transition, params, query, callback) {
       UserStore.initialize();
       AppHandlerViewActionCreators.loadApp();
       callback();
     }
+};
+
+const AppHandler = React.createClass({
+  propTypes: {
+    data: PropTypes.shape({
+      user: PropTypes.instanceOf(Immutable.Map)
+    })
   },
 
-  stores: [UserStore],
-
   mixins: [
-    StoresMixin,
     Navigation,
     State
   ],
@@ -53,30 +67,30 @@ const AppHandler = React.createClass({
       false
     );
 
-    if (!isInside && isEmpty(params) && this.state.user.get('auth')) {
+    if (!isInside && isEmpty(params) && this.props.data.user.get('auth')) {
       this.replaceWith('habits');
-    } else if ((isInside || !isEmpty(params)) && !this.state.user.get('auth')) {
+    } else if ((isInside || !isEmpty(params)) && !this.props.data.user.get('auth')) {
       this.replaceWith('front');
     }
   },
 
-  componentDidUpdate(prevProps: any, prevState: any) {
-    if (!prevState.user.get('auth') && this.state.user.get('auth')) {
+  componentDidUpdate(prevProps: any) {
+    if (!prevProps.data.user.get('auth') && this.props.data.user.get('auth')) {
       this.replaceWith('habits');
-    } else if (prevState.user.get('auth') && !this.state.user.get('auth')) {
+    } else if (prevProps.data.user.get('auth') && !this.props.data.user.get('auth')) {
       this.replaceWith('front');
     }
   },
 
   render(): any {
-    const insideHandler = this.state.user.get('user') ?
-      <RouteHandler user={this.state.user} key="inside" /> : null;
+    const insideHandler = this.props.data.user.get('user') ?
+      <RouteHandler user={this.props.data.user} key="inside" /> : null;
 
-    const content = this.state.user.get('auth') ?
-      <Inside user={this.state.user}>
+    const content = this.props.data.user.get('auth') ?
+      <Inside user={this.props.data.user}>
         {insideHandler}
       </Inside> :
-      <Outside user={this.state.user}>
+      <Outside user={this.props.data.user}>
         <RouteHandler key="outside" />
       </Outside>;
 
@@ -88,4 +102,7 @@ const AppHandler = React.createClass({
   }
 });
 
-module.exports = AppHandler;
+module.exports = compose(
+  AddStatics(statics),
+  ConnectToStores(stores, getStateFromStores)
+)(AppHandler);
