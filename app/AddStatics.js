86eef3848a48d078bbd2137872575b1773e2eeff
Franky Chung <franchun@gmail.com>
2015-03-21 18:45:22 +0900

    AddStatics

---
+++ app/AddStatics.js
@@ -0,0 +1,22 @@
+/**
+ * @flow
+ */
+
+const React = require('react/addons');
+
+const AddStatics: (
+  x: Object,
+  y: React.Component
+) => React.Component = (statics, ComposedComponent) => {
+  const Enhanced = React.createClass({
+    statics,
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  });
+
+  return Enhanced;
+};
+
+module.exports = AddStatics;
+
