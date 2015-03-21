5ff34af499056bcaedb9032980d2f90cde9a3989
Franky Chung <franchun@gmail.com>
2015-03-21 18:45:06 +0900

    Fix tabs in PureRender

--- app/PureRender.js
+++ app/PureRender.js
@@ -6,14 +6,14 @@ const React = require('react/addons');
 const { PureRenderMixin } = React.addons;
 
 const PureRender: (x: React.Component) => React.Component = ComposedComponent => {
-	const Enhanced = React.createClass({
-		mixins: [PureRenderMixin],
-		render() {
-			return <ComposedComponent {...this.props} />;
-		}
-	});
+  const Enhanced = React.createClass({
+    mixins: [PureRenderMixin],
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  });
 
-	return Enhanced;
+  return Enhanced;
 };
 
 module.exports = PureRender;
