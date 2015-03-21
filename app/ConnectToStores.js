6d6a4ebb069ed298c39cc8e089b26669c525c655
Franky Chung <franchun@gmail.com>
2015-03-21 18:35:49 +0900

    Add PureRender, remove class from ConnectToStores

--- app/ConnectToStores.js
+++ app/ConnectToStores.js
@@ -4,44 +4,40 @@
 
 const React = require('react/addons');
 const each = require('lodash/collection/each');
-
-const { Component } = React;
+const curry = require('lodash/function/curry');
 
 /*::
 type Stores = Array<Object>;
 */
 
 const ConnectToStores: (
-	x: Component,
-	y: Stores,
-	z: Function
-) => Component = (ComposedComponent, stores, getStateFromStores) => {
-	class Enhanced extends Component {
-		constructor() {
-			this.state = getStateFromStores();
-			this._onChange = this._onChange.bind(this);
-		}
+	x: Stores,
+	y: Function,
+	z: React.Component
+) => React.Component = (stores, getStateFromStores, ComposedComponent) => {
+	const Enhanced = React.createClass({
+		getInitialState() {
+			return getStateFromStores();
+		},
 
 		componentDidMount() {
 			each(stores, store => store.addChangeListener(this._onChange));
-		}
+		},
 
 		componentWillUnmount() {
 			each(stores, store => store.removeChangeListener(this._onChange));
-		}
+		},
 
 		_onChange() {
 			this.setState(getStateFromStores());
-		}
+		},
 
 		render() {
 			return <ComposedComponent {...this.props} data={this.state} />;
 		}
-	}
+	});
 
 	return Enhanced;
 };
 
-module.exports = ConnectToStores;
-
-
+module.exports = curry(ConnectToStores);
