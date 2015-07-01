3c2bf1c0853594ef17c588a81c1ef225b67d6e17
Franky Chung <franchun@gmail.com>
2015-07-01 20:22:51 +0900

    Burn everything!

--- app/index.js
+++ app/index.js
@@ -1,11 +1,2 @@
-/**
- * @flow
- */
+/* @flow */
 
-const React = require('react/addons');
-const Router = require('react-router');
-const routes = require('./routes');
-
-module.exports = Router.run(routes, Router.HistoryLocation, Handler => {
-  React.render(<Handler />, document.getElementById('content'));
-});
